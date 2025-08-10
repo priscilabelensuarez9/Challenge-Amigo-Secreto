9753f4084d0ab99d3d1fbbb54ca9a23f6a7aa30d
Priscila Suarez <priscilabelensuarez9@gmai.com>
2025-08-10 20:29:43 -0300

    crea array

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let NombreAmigos = [];
+
+function agregarAmigo() {
+    let nombre = document.getElementById("amigo").value;
+    console.log(nombre);
+    NombreAmigos.push(nombre);
+    document.getElementById("amigo").value = ""; // Limpia el input después de agregar
+}
+
