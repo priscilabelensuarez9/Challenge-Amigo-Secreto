2df0723c370b0543626ab684f8b98f422e984eac
Priscila Suarez <priscilabelensuarez9@gmai.com>
2025-08-17 10:19:27 -0300

    Genera Lista de amigos y sorteo de amigos

--- app.js
+++ app.js
@@ -5,12 +5,32 @@ function agregarAmigo() {
     console.log(nombre);
     NombreAmigos.push(nombre);
     document.getElementById("amigo").value = ""; // Limpia el input después de agregar
+    ListarAmigos(); // Llama a la función para actualizar la lista
 }
 
-function mostrarLista(elemento, texto) {
-document.getElementById("listaDeAmigos").value = `${agregarAmigo}`;
-if (NombreAmigos.length === 0) {
-    elemento.innerHTML = "Por favor, inserte un nombre.";
-    
+
+
+function ListarAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = ""; // Limpia la lista antes de agregar
+    for (let i = 0; i < NombreAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = NombreAmigos[i];
+        lista.appendChild(li);
+    }
 }
+
+function sortearAmigo() {
+    if (NombreAmigos.length === 0) {
+        alert("Por favor, inserte un nombre.");
+        return;
+    } else {
+        let amigoSeleccionado = NombreAmigos[Math.floor(Math.random() * NombreAmigos.length)];
+      let resultadoSorteo = document.getElementById("resultado");
+       resultadoSorteo.textContent = "El amigo secreto es: " + amigoSeleccionado;
+    innerHTML = ""; // Limpia el input después de sortear
+    ListarAmigos(); // Llama a la función para actualizar la lista
+    }
 }
+
+
